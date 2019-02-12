f4882b820bc41315b181598727b46d7c9503d5fa
Fusiakja <fusiakjakub@gmx.net>
2019-02-12 17:40:35 +0100

    Update installBayes.R

--- installBayes.R
+++ installBayes.R
@@ -1,12 +1,1 @@
-source("https://bioconductor.org/biocLite.R")
-biocLite("graph")
-biocLite("RBGL")
-biocLite("Rgraphviz")
-
 install.packages("rjags") 
-install.packages("HydeNet") 
-
-install.packages("gRain")
-install.packages("gRim")
-
-install.packages("bnlearn")
