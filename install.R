12ccce483c7c1315a73c4feda276bd8fc21caff6
Fusiakja <fusiakjakub@gmx.net>
2019-02-12 15:48:43 +0100

    Update install.R

--- install.R
+++ install.R
@@ -4,3 +4,16 @@ install.packages("httr")
 install.packages("shinydashboard")
 install.packages('leaflet')
 install.packages('plotly')
+
+source("https://bioconductor.org/biocLite.R")
+biocLite("graph")
+biocLite("RBGL")
+biocLite("Rgraphviz")
+
+install.packages("rjags") 
+install.packages("HydeNet") 
+
+install.packages("gRain")
+install.packages("gRim")
+
+install.packages("bnlearn")
