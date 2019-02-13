c975486530c9473515ac31fa721ced70724cb245
Fusiakja <fusiakjakub@gmx.net>
2019-02-13 14:20:25 +0100

    Update install.R

--- install.R
+++ install.R
@@ -1,1 +1,12 @@
-install.packages("rjags") 
+source("https://bioconductor.org/biocLite.R")
+biocLite("graph")
+biocLite("RBGL")
+biocLite("Rgraphviz")
+
+install.packages("rjags", repos="https://cloud.r-project.org/", dependencies=TRUE, clean=TRUE) 
+install.packages("HydeNet", repos="https://cloud.r-project.org/", dependencies=TRUE, clean=TRUE) 
+
+install.packages("gRain", repos="https://cloud.r-project.org/", dependencies=TRUE, clean=TRUE)
+install.packages("gRim", repos="https://cloud.r-project.org/", dependencies=TRUE, clean=TRUE)
+
+install.packages("bnlearn", repos="https://cloud.r-project.org/", dependencies=TRUE, clean=TRUE)
